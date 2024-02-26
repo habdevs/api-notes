c1002dcbac5963bccc6c8f4d1c2d520a517797b7
habdevs <habdevs@yandex.ru>
2024-02-26 22:05:31 +0500

    feat: added model field

--- src/models/post.js
+++ src/models/post.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
 	title: { type: String, required: true },
-	text: { type: String, required: true },
+	content: { type: String, required: true },
+	tags: { type: String, required: true },
 	images: [{ type: String }],
 }, {
 	timestamp: true
