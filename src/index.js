6893210f88c931af3081e85bbde3f7e66572ee5c
habdevs <habdevs@yandex.ru>
2024-02-27 12:23:25 +0500

    feat: model refinement | updating the database connection on mongoose

--- src/index.js
+++ src/index.js
@@ -1,86 +1,72 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+import Post from './models/post.js'; // Предполагается, что ваша модель находится в файле ./models/post.js
 
-const uri = 'mongodb+srv://ahramickih:TOEToCA84ap9a69X@api-notes.rlncm4z.mongodb.net/?retryWrites=true&w=majority&appName=api-notes'
+const uri =
+  'mongodb+srv://ahramickih:TOEToCA84ap9a69X@api-posts.rlncm4z.mongodb.net/?retryWrites=true&w=majority&appName=api-posts';
 const port = process.env.PORT || 4000;
-
-const client = new MongoClient(uri);
-
-let notes = [
-  {
-    id: '1',
-    content: 'This is a note',
-    author: 'Bob Harley'
-  },
-  {
-    id: '2',
-    content: 'This is another note',
-    author: 'Harlow Everly'
-  },
-  {
-    id: '3',
-    content: 'Oh hey look, another note!',
-    author: 'Riley Harrison'
-  }
-];
 
 async function startApolloServer() {
   const typeDefs = gql`
-  type Note {
-    id: ID
-    content: String
-    author: String
-  }
+    type Post {
+      id: ID!
+      title: String!
+      content: String!
+      tags: String!
+      author: String!
+      createdAt: String
+    }
 
-  type Query {
-    hello: String
-		kek: String
-    notes: [Note]
-    note(id: ID): Note
-  }
+    type Query {
+      posts: [Post]
+      post(id: ID!): Post
+    }
 
-  type Mutation {
-    newNote(content: String!, author: String!): Note
-  }
-`;
+    type Mutation {
+      newPost(
+        title: String!
+        content: String!
+        tags: String!
+        author: String!
+        createdAt: String
+      ): Post
+    }
+  `;
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    kek: () => 'LOL kek',
-    notes: async () => {
-      const notesCollection = client.db().collection('notes');
-      const notes = await notesCollection.find().toArray();
-      return notes;
+  const resolvers = {
+    Query: {
+      posts: async () => {
+        return await Post.find();
+      },
+      post: async (parent, args) => {
+        return await Post.findById(args.id);
+      },
     },
-    note: async (parent, args) => {
-      const notesCollection = client.db().collection('notes');
-      return notesCollection.findOne({ id: args.id });
+    Mutation: {
+      newPost: async (parent, args) => {
+        const newPost = new Post({
+          title: args.title,
+          content: args.content,
+          tags: args.tags,
+          author: args.author,
+        });
+        return await newPost.save();
+      },
     },
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      const notesCollection = client.db().collection('notes');
-      const noteValue = {
-        id: String((await notesCollection.countDocuments()) + 1),
-        content: args.content,
-        author: args.author,
-      };
-      await notesCollection.insertOne(noteValue);
-      return noteValue;
-    },
-  },
-};
-
+  };
+  
   const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
-
+  
   const app = express();
   server.applyMiddleware({ app, path: '/api' });
-
-  await client.connect();
-
+  
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
   app.listen({ port }, () =>
     console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`)
   );
--- src/models/post.js
+++ src/models/post.js
@@ -1,13 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const postSchema = new mongoose.Schema({
-	title: { type: String, required: true },
-	content: { type: String, required: true },
-	tags: { type: String, required: true },
-	images: [{ type: String }],
-}, {
-	timestamp: true
-});
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: { type: String, required: true },
+    images: { type: String },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Post = mongoose.model('Post', postSchema);
 
